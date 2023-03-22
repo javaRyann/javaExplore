3bd8dc345693f033840768420ebf11733b7076f9
javaRyann <597162433@qq.com>
2023-03-22 23:52:17 +0800

    11

--- xl-2023-nacos/consumer/src/main/java/com/learning/xiaol/consumer/controller/ConsumerController.java
+++ xl-2023-nacos/consumer/src/main/java/com/learning/xiaol/consumer/controller/ConsumerController.java
@@ -2,6 +2,7 @@ package com.learning.xiaol.consumer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +16,7 @@ public class ConsumerController {
     RestTemplate restTemplate;
 
     @GetMapping("/hi")
-    public String hi(String name) {
+    public String hi(@RequestParam("name") String name) {
         return restTemplate.getForObject("http://service-provider/provide-test/hello",String.class,name);
     }
 }
--- xl-2023-nacos/provider/src/main/java/com/learning/xiaol/provider/Controller/ProviderController.java
+++ xl-2023-nacos/provider/src/main/java/com/learning/xiaol/provider/Controller/ProviderController.java
@@ -1,15 +1,20 @@
 package com.learning.xiaol.provider.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/provide-test")
 public class ProviderController {
 
+    @Value("${server.port}")
+    String port;
+
     @GetMapping("/hello")
-    public String hello (String name) {
-        return "Hi Nacos,I'm " + name;
+    public String hello (@RequestParam("name") String name) {
+        return "Hi Nacos:" + port +",I'm " + name;
     }
 }
