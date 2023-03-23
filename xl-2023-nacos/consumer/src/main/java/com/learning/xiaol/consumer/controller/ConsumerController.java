826e088f99dca5a9741eb60174e0fd24552bcc74
javaRyann <597162433@qq.com>
2023-03-23 23:33:41 +0800

    init

--- xl-2023-nacos/consumer/src/main/java/com/learning/xiaol/consumer/controller/ConsumerController.java
+++ xl-2023-nacos/consumer/src/main/java/com/learning/xiaol/consumer/controller/ConsumerController.java
@@ -1,9 +1,9 @@
 package com.learning.xiaol.consumer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -12,11 +12,17 @@ import javax.annotation.Resource;
 @RequestMapping("/consumer-test")
 public class ConsumerController {
 
+    Logger logger = LoggerFactory.getLogger(ConsumerController.class);
+
+    @Value("${server.port}")
+    String port;
+
     @Resource
     RestTemplate restTemplate;
 
-    @GetMapping("/hi")
-    public String hi(@RequestParam("name") String name) {
-        return restTemplate.getForObject("http://service-provider/provide-test/hello",String.class,name);
+    @GetMapping(value = "/hi")
+    public String rest(@RequestParam(value = "name") String name) {
+        logger.info("rest port: {}", port);
+        return restTemplate.getForObject("http://service-provider/provide-test/hello/?name={name}", String.class, name);
     }
 }
