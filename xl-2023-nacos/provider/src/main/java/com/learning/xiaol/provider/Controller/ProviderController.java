a2deb92e518d11aed26cb50d29096f1bf413944a
javaRyann <597162433@qq.com>
2023-03-25 23:48:45 +0800

    init

--- xl-2023-nacos/provider/src/main/java/com/learning/xiaol/provider/Controller/ProviderController.java
+++ xl-2023-nacos/provider/src/main/java/com/learning/xiaol/provider/Controller/ProviderController.java
@@ -1,12 +1,14 @@
 package com.learning.xiaol.provider.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope // 支持Nacos动态刷新
 @RequestMapping("/provide-test")
 public class ProviderController {
 
@@ -16,5 +18,10 @@ public class ProviderController {
     @GetMapping("/hello")
     public String hello (@RequestParam("name") String name) {
         return "Hi Nacos :" + port +",I'm " + name;
+    }
+
+    @GetMapping("/testConfig")
+    public String testConfig () {
+        return "config.filePath: " + port;
     }
 }
--- xl-2023-nacos/provider/src/main/java/com/learning/xiaol/provider/ProviderApplication.java
+++ xl-2023-nacos/provider/src/main/java/com/learning/xiaol/provider/ProviderApplication.java
@@ -1,14 +1,27 @@
 package com.learning.xiaol.provider;
 
+import org.bouncycastle.util.Times;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class ProviderApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProviderApplication.class, args);
+    private static final Logger logger = LoggerFactory.getLogger(ProviderApplication.class);
+
+    public static void main(String[] args) throws Exception{
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ProviderApplication.class, args);
+        while (true) {
+            String port = applicationContext.getEnvironment().getProperty("userName");
+            logger.info("xl-2023-nacos-provider-config: config.userName : {}", port);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
 }
